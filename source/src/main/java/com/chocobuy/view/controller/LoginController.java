9fe1a7a956086e8a9f38c0b4dab16439dce3b345
Hernameis <msh4287@naver.com>
2022-05-03 18:11:44 +0900

    unused SessionAttribute annotation deleted

--- source/src/main/java/com/chocobuy/view/controller/LoginController.java
+++ source/src/main/java/com/chocobuy/view/controller/LoginController.java
@@ -1,29 +1,21 @@
 package com.chocobuy.view.controller;
 
-import java.util.Date;
-
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.util.WebUtils;
 
 import com.chocobuy.biz.user.UserService;
 import com.chocobuy.biz.user.UserVO;
 import com.chocobuy.biz.util.CertifiedPhoneNumber;
 
 @Controller
-@SessionAttributes("UserInfo")
 public class LoginController {
 	@Autowired
 	private UserService userService;
@@ -181,7 +173,4 @@ public class LoginController {
 		}
 		return "redirect:/Login/login";
 	}
-	
-
-		
 }
