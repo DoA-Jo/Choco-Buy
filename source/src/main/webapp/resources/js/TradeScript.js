bbd0b6046761316dbd5b1a7f2b8c11eac6359a06
gimae1 <97872420+gimae1@users.noreply.github.com>
2022-05-14 13:34:58 +0900

    Update TradeScript.js
    오타 수정

--- source/src/main/webapp/resources/js/TradeScript.js
+++ source/src/main/webapp/resources/js/TradeScript.js
@@ -38,7 +38,7 @@
  
         var NumRegExp = /^[0-9]{2-9}$/;
         if (!NumRegExp.test(Num)) {
-            alert("금액dl 올바르지 않습니다.");
+            alert("금액이 올바르지 않습니다.");
             return false;
         }
         return true; //확인이 완료되었을 때
